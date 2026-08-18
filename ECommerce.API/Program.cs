7068ab7d05f4cd07ae491a7a5fbd10b0acdaed93
akysawant <112846996+akysawant@users.noreply.github.com>
2026-08-18 21:30:10 +0530

    Prepare application for AWS deployment

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -147,12 +147,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AngularPolicy");
 
